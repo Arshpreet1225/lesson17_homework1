c3156e9b76b54b9ab2e2b67e1ab76b2a24409119
Arshpreet1225 <kaurarshpreet1225@gmail.com>
2026-06-26 23:47:26 +0530

    homework.commit

---
+++ main.py
@@ -0,0 +1,12 @@
+bill_amount=int(input("enter the amount of total bill:"))
+paid_amount=int(input("enter the amount you have paid:"))
+while paid_amount>0:
+    if paid_amount<=0:
+        pass
+    elif paid_amount<=bill_amount:
+        print("you have a due")
+        continue
+    elif paid_amount==bill_amount:
+        break
+    else:
+        print("invalid")
