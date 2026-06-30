5295d85c2dd32dc6d5e049fbabae1fbbc7c1d768
Arshpreet1225 <kaurarshpreet1225@gmail.com>
2026-06-30 23:55:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ bill_amount=int(input("enter the amount of total bill:"))
 paid_amount=int(input("enter the amount you have paid:"))
 while paid_amount>0:
     if paid_amount<=0:
-        pass
+        break
     elif paid_amount<=bill_amount:
         print("you have a due")
         continue
     elif paid_amount==bill_amount:
-        break
+        print("you have paid the full amount")
+        pass
     else:
         print("invalid")
